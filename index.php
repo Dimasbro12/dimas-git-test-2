73195cefc31dcb964742269e72847d00414fa084
webprogrammingunpas <dwid35895@gmail.com>
2024-01-27 12:02:48 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -7,6 +7,11 @@
     <body>
         <h1>Jello WORLD</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat fugit animi velit corporis. Possimus at quas ut vel asperiores voluptas ducimus repudiandae, eum aliquam voluptate rerum culpa delectus voluptatem consectetur.</p>
+    <ul>
+        <li>Dimas</li>
+        <li>Dwi</li>
+        <li>Pramono</li>
+    </ul>
     </body>
 
 </html>
